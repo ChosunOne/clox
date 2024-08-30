a64c38ff6bdcbce7e2c5992caabc26a60a6c1674
ChosunOne <chosunone@protonmail.com>
2024-08-30 23:34:23 +0900

    feat: strings

--- src/vm.c
+++ src/vm.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+
 #include "compiler.h"
 #include "chunk.h"
 #include "common.h"
+#include "memory.h"
 #include "vm.h"
 #include "debug.h"
+#include "object.h"
 #include "value.h"
 
 VM vm;
 
 static void resetStack() {
         vm.stackTop = vm.stack;
+        vm.objects = NULL;
 }
 
 static void runtimeError(const char* format, ...) {
@@ -31,7 +36,7 @@ void initVM() {
 }
 
 void freeVM() {
-
+        freeObjects();
 }
 
 void push(Value value) {
@@ -50,6 +55,20 @@ static Value peek(int distance) {
 
 static bool isFalsey(Value value) {
         return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static void concatenate() {
+        ObjString* b = AS_STRING(pop());
+        ObjString* a = AS_STRING(pop());
+
+        int length = a->length + b->length;
+        char* chars = ALLOCATE(char, length + 1);
+        memcpy(chars, a->chars, a->length);
+        memcpy(chars + a->length, b->chars, b->length);
+        chars[length] = '\0';
+
+        ObjString* result = takeString(chars, length);
+        push(OBJ_VAL(result));
 }
 
 static InterpretResult run() {
@@ -103,7 +122,16 @@ static InterpretResult run() {
                                 break;
                         }
                         case OP_ADD: {
-                                BINARY_OP(NUMBER_VAL, +);
+                                if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+                                        concatenate();
+                                } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+                                        double b = AS_NUMBER(pop());
+                                        double a = AS_NUMBER(pop());
+                                        push(NUMBER_VAL(a + b));
+                                } else {
+                                        runtimeError("Operands must be two numbers or two strings.");
+                                        return INTERPRET_RUNTIME_ERROR;
+                                }
                                 break;
                         }
                         case OP_SUBTRACT: {
