50df63f58e28f0d2500a844ae222751b48492dfe
ChosunOne <chosunone@protonmail.com>
2024-09-02 16:23:28 +0900

    feat: garbage collector

--- src/compiler.c
+++ src/compiler.c
@@ -6,10 +6,12 @@
 #include "chunk.h"
 #include "common.h"
 #include "compiler.h"
+#include "memory.h"
+#include "scanner.h"
+
 #ifdef DEBUG_PRINT_CODE
         #include "debug.h"
 #endif
-#include "scanner.h"
 
 typedef struct {
         Token current;
@@ -772,4 +774,12 @@ ObjFunction* compile(const char* source) {
         }
         ObjFunction* function = endCompiler();
         return parser.hadError ? NULL : function;
+}
+
+void markCompilerRoots() {
+        Compiler* compiler = current;
+        while (compiler != NULL) {
+                markObject((Obj*)compiler->function);
+                compiler = compiler->enclosing;
+        }
 }
